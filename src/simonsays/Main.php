1df930af25e3be9b7789be3bf733ea4758724250
Jelly9912 <taytan9912@gmail.com>
2015-11-14 09:38:47 +0100

    Create Main.php

---
+++ src/simonsays/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace simonsays;
+
+use pocketmine\player\Player;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\utils\Random;
+use pocketmine\math\Vector3 as Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+ public function onEnable() {
+  $this->getLogger()->info("SimonSays enabled");
+  }
+  
+ public function onDisable() {
+  $this->getLogger()->info("SimonSays disabled");
+  }
+}
